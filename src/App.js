7ae499c00a0637a32b13f30ae9eb104765a4ff76
Vytautas Sluckas <v.sluckas@gmail.com>
2020-09-25 19:37:31 +0300

    adding drawing box on the face feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 
-function App() {
-  return (
+
+const app = new Clarifai.App({
+  apiKey:'7acfdc0c1f504a509815bf9b537daeac'
+})
+
+const particlesOptions ={
+  
+  particles: {
+    number: {
+      value: 30,
+      density:{
+        enable: true,
+        value_area:800
+      }
+    }
+  }
+};
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+
+    }
+  }
+calculateFaceLocation = (data) => {
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  return {
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row* height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.bottom_row * height),
+  }
+
+}
+
+displayFaceBox = (box) => {
+  console.log(box);
+  this.setState({box: box});
+}
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onButtonSubmit = (event) => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
+      // console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+  }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className='particles'
+              params={particlesOptions}
+            />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm 
+        onInputChange={this.onInputChange}
+        onButtonSubmit={this.onButtonSubmit}
+      />
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div className='ma4 mt0'>
+            <div className='white f3'>
+                {' Vychek your current ranks is ...'}
+            </div>
+            <div className='white f1'>
+                {'#2'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
